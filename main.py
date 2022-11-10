662aabd1d7266898c3f863a318925c0a209fa301
SHOUON YOSHIDA <s18k364@keio.jp>
2022-11-10 12:19:06 +0900

    グラフを整えた

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ def main():
     #グラフの作成
     fig = Figure()
     ax = fig.add_subplot(1, 1, 1)
+    ax.set_title('$y=\\sin(\\pi x)$')
+    ax.set_xlabel('$x$')
+    ax.set_ylabel('$y$')
+    ax.axhline(color='#777777')
+    ax.axvline(color='#777777')
     ax.plot(x,y)
     fig.savefig('out.png')
 

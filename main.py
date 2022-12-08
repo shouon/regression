5f3b91e077f674bc5c97973e4f68fe362050ac8f
SHOUON YOSHIDA <s18k364@keio.jp>
2022-12-08 10:08:09 +0900

    スコア計算、グラフ出力を関数化した

--- main.py
+++ main.py
@@ -2,6 +2,39 @@ import numpy as np
 from matplotlib.figure import Figure
 import japanize_matplotlib as _
 
+def calculate_score(y, y_pred, eps_score):
+    norm_diff = np.sum(np.abs(y-y_pred))
+    norm_y=np.sum(np.abs(y))
+    score = norm_diff/(norm_y + eps_score)
+    return score
+def save_graph(
+    xy = None, 
+    xy_sample=None,
+    xy_pred=None,
+    title=None,
+    filename = 'out.png'
+):
+    fig = Figure()
+    ax = fig.add_subplot(1, 1, 1)
+    if title is not None:
+        ax.set_title(title)
+    ax.set_xlabel('$x$')
+    ax.set_ylabel('$y$')
+    ax.axhline(color='#777777')
+    ax.axvline(color='#777777')
+    if xy is not None:
+        x, y=xy
+        ax.plot(x,y,label='真の関数　$f$')
+    if xy_sample is not None:
+        x_sample, y_sample=xy_sample
+        ax.scatter(x_sample, y_sample, color='red', label='学習サンプル')
+    if xy_pred is not None:
+        x, y_pred=xy_pred
+        ax.plot(x,y_pred,label='回帰関数　$\\hat{f}$')
+ 
+    ax.legend()
+    fig.savefig(filename)
+
 def main():
     # 実験条件
     x_min = -1
@@ -33,23 +66,14 @@ def main():
     ## yの予測値を計算
     y_pred = np.squeeze((x[:, np.newaxis]**p) @ a)
     #評価指標の計算
-    norm_diff = np.sum(np.abs(y-y_pred))
-    norm_y=np.sum(np.abs(y))
-    score = norm_diff/(norm_y + eps_score)
+    score = calculate_score(y, y_pred, eps_score)
     print(f'{score=: .3f}')
     #グラフの作成
-    fig = Figure()
-    ax = fig.add_subplot(1, 1, 1)
-    ax.set_title('$y=\\sin(\\pi x)$')
-    ax.set_xlabel('$x$')
-    ax.set_ylabel('$y$')
-    ax.axhline(color='#777777')
-    ax.axvline(color='#777777')
-    ax.plot(x,y,label='真の関数　$f$')
-    ax.scatter(x_sample, y_sample, color='red', label='学習サンプル')
-    ax.plot(x,y_pred,label='回帰関数　$\\hat{f}$')
-    ax.legend()
-    fig.savefig('out.png')
+    save_graph(
+        xy = (x,y), xy_sample=(x_sample,y_sample), xy_pred=(x, y_pred),
+        title=r'$y = \sin (\pi x)$'
+    )
+    
 
 if __name__== '__main__':
     main()
